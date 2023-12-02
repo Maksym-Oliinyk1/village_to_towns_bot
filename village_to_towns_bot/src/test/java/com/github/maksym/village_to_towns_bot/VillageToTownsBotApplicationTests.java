45020a9c658863b19a60a5a9c312a91dedfb50aa
Maksym Oliinyk <spatium.profugo@gmail.com>
2023-12-02 23:46:14 +0200

    Create Spring Initializer

---
+++ village_to_towns_bot/src/test/java/com/github/maksym/village_to_towns_bot/VillageToTownsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maksym.village_to_towns_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageToTownsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
